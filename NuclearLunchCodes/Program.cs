ec5d328b66664bef7a73b4e696aa5db0edda3de9
The Dude <The Dude@THEDUDE>
2017-02-21 22:24:51 +0100

    Nu som object

--- NuclearLunchCodes/Program.cs
+++ NuclearLunchCodes/Program.cs
@@ -8,8 +8,31 @@ namespace NuclearLunchCodes
 {
     class Program
     {
+
         static void Main(string[] args)
         {
+            int[] Disarray = new int[] { 8, 12, 3, 20, 7, 8, 12, 15, 2, 10, 33, 14, 3, 20 };
+            drawarray x = new drawarray();
+            x.draw(Disarray);
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+        
+    }
+     class drawarray
+    {
+        public void draw(int[] k)
+        {
+            
+            for (int i = 0; i < k.Length; i++)
+            {
+                Console.WriteLine("");
+                for (int j = 0; j < k[i]; j++)
+                {
+                    Console.Write("-");
+                }
+
+            }
         }
     }
 }
